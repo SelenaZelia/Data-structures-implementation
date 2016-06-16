f0a75ff3d795c161c12de43736fd832bdc9882f5
Alex Zelia <selena.zelia@gmail.com>
2016-06-16 11:57:27 +0200

    More tests, more commenting, MOAR

--- DataStructuresImplementation/src/com/zelia/ds/Program.java
+++ DataStructuresImplementation/src/com/zelia/ds/Program.java
@@ -4,17 +4,25 @@ import com.zelia.ds.map.CustomMap;
 
 public class Program {
 	public static void main(String[] args) {
-		/*CUSTOM MAP*/
+		/* CUSTOM MAP */
 		CustomMap<String, String> cmap = new CustomMap<>();
 		cmap.put("Test1", "Value2");
 		cmap.put("Test1", "Value1");
 		cmap.put("Test2", "Value2");
 		cmap.put("Test3", "Value3");
 		cmap.put("Test4", "Value4");
-		cmap.remove("Test4");		
-		System.out.println(cmap);
+		cmap.remove("Test4");
+		System.out.println("Map : " + cmap);
 		System.out.println(cmap.get("Test2"));
-		System.out.println(cmap.keySet().size() + " keys");
-		
+		System.out.println(cmap.keySet().size() + " keys (keyset)" + cmap.size() + " elements (size) "
+				+ (cmap.isEmpty() ? "Empty" : "Not empty"));
+
+		System.out.println("\nClearing the map\n");
+		cmap.Clear();
+		System.out.println("Map : " + cmap);
+		System.out.println(cmap.get("Test2"));
+		System.out.println(cmap.keySet().size() + " keys (keyset)" + cmap.size() + " elements (size) "
+				+ (cmap.isEmpty() ? "Empty" : "Not empty"));
+
 	}
 }
--- DataStructuresImplementation/src/com/zelia/ds/map/CustomMap.java
+++ DataStructuresImplementation/src/com/zelia/ds/map/CustomMap.java
@@ -4,12 +4,69 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+ * Minimal implementation of a map
+ * A map represents a data structure in which 
+ * collections of unique key and collections 
+ * of values are stored where each key is 
+ * associated with one value. 
+ */
 public class CustomMap<K, V> {
+	// Elements count
 	private int size;
 	private int DEFAULT_CAPACITY = 16;
 	@SuppressWarnings("unchecked")
 	private CustomEntry<K, V>[] values = new CustomEntry[DEFAULT_CAPACITY];
 
+	public void Clear() {
+		for (int i = 0; i < size; ++i) {
+			values[i] = null;
+			values = Arrays.copyOf(values, DEFAULT_CAPACITY);
+		}
+		size = 0;
+	}
+
+	/*
+	 * Make sure there is no null element between two entry
+	 */
+	private void condenseArray(int start) {
+		for (int i = start; i < size; ++i)
+			values[i] = values[i + 1];
+	}
+
+	/*
+	 * Return true if the map already got an entry using that key
+	 */
+	public boolean containsKey(K key) {
+		for (int i = 0; i < size; ++i)
+			if (values[i].getKey().equals(key))
+				return true;
+		return false;
+	}
+
+	/*
+	 * Return true if the map already got an entry using that value
+	 */
+	public boolean containsValue(V value) {
+		for (int i = 0; i < size; ++i)
+			if (values[i].getValue().equals(value))
+				return true;
+		return false;
+	}
+
+	/*
+	 * Verify that the array isn't full, if it is : double capacity
+	 */
+	private void ensureCapacity() {
+		if (size == values.length) {
+			int newSize = values.length * 2;
+			values = Arrays.copyOf(values, newSize);
+		}
+	}
+
+	/*
+	 * Return the value associated to the key
+	 */
 	public V get(K key) {
 		for (int i = 0; i < size; ++i)
 			if (values[i] != null)
@@ -18,32 +75,47 @@ public class CustomMap<K, V> {
 		return null;
 	}
 
+	/*
+	 * Check that the array is empty > ? check all the array or just the size ?
+	 * <
+	 */
+	public boolean isEmpty() {
+		return (size == 0) ? true : false;
+	}
+
+	/*
+	 * Return the keyset
+	 */
+	public Set<K> keySet() {
+		Set<K> set = new HashSet<>();
+		for (int i = 0; i < size; ++i) {
+			set.add(values[i].getKey());
+		}
+		return set;
+	}
+
+	/*
+	 * Add a new entry
+	 */
 	public void put(K key, V value) {
 		boolean insert = true;
+		// if it already exists, just update the value
 		for (int i = 0; i < size; ++i) {
 			if (values[i].getKey().equals(key)) {
 				values[i].setValue(value);
 				insert = false;
 			}
 		}
-
+		// else create a new one
 		if (insert) {
-			ensureCapacity();
+			ensureCapacity(); // double the capacity if needed
 			values[size++] = new CustomEntry<K, V>(key, value);
 		}
 	}
 
-	private void ensureCapacity() {
-		if (size == values.length) {
-			int newSize = values.length * 2;
-			values = Arrays.copyOf(values, newSize);
-		}
-	}
-
-	public int size() {
-		return size;
-	}
-
+	/*
+	 * Remove an entry
+	 */
 	public void remove(K key) {
 		for (int i = 0; i < size; ++i) {
 			if (values[i].getKey().equals(key)) {
@@ -54,22 +126,19 @@ public class CustomMap<K, V> {
 		}
 	}
 
-	private void condenseArray(int start) {
-		for (int i = start; i < size; ++i) 
-			values[i] = values[i + 1]; 
+	/*
+	 * Return the element count
+	 */
+	public int size() {
+		return size;
 	}
 
-	public Set<K> keySet() {
-		Set<K> set = new HashSet<>();
-		for (int i = 0; i < size; ++i) {
-			set.add(values[i].getKey());
-		}
-		return set;
-	}
-	
-	public String toString(){
+	/*
+	 * Just used for testing
+	 */
+	public String toString() {
 		String s = "";
-		for(int i = 0 ; i < size; ++i){
+		for (int i = 0; i < size; ++i) {
 			s += values[i].getKey() + ": " + values[i].getValue() + " ; ";
 		}
 		return s;
